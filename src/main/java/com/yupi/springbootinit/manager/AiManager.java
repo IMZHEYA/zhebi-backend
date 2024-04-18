92c0b0fbbe844635170afc1d5f52c343d11e063a
Serendipityzhezi <1342558165@qq.com>
2024-04-18 20:02:05 +0800

    引入讯飞星火认知大模型并测试调用

--- src/main/java/com/yupi/springbootinit/manager/AiManager.java
+++ src/main/java/com/yupi/springbootinit/manager/AiManager.java
@@ -6,18 +6,31 @@ import com.yupi.yucongming.dev.client.YuCongMingClient;
 import com.yupi.yucongming.dev.common.BaseResponse;
 import com.yupi.yucongming.dev.model.DevChatRequest;
 import com.yupi.yucongming.dev.model.DevChatResponse;
+import io.github.briqt.spark4j.SparkClient;
+import io.github.briqt.spark4j.constant.SparkApiVersion;
+import io.github.briqt.spark4j.model.SparkMessage;
+import io.github.briqt.spark4j.model.SparkSyncChatResponse;
+import io.github.briqt.spark4j.model.request.SparkRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.prefs.BackingStoreException;
 
 /**
  * 用于对接AI平台
  */
 @Service
+@Slf4j
 public class AiManager {
     @Resource
     private YuCongMingClient yuCongMingClient;
+
+
+    @Resource
+    private SparkClient sparkClient;
 
     public String doChat(long modelId,String message){
         DevChatRequest devChatRequest = new DevChatRequest();
@@ -29,5 +42,31 @@ public class AiManager {
         }
         return response.getData().getContent();
     }
+
+    /**
+     * 向讯飞 AI 发送请求
+     *
+     * @return
+     */
+    public String sendMesToAIUseXingHuo(final String content) {
+        List<SparkMessage> messages = new ArrayList<>();
+        messages.add(SparkMessage.userContent(content));
+        // 构造请求
+        SparkRequest sparkRequest = SparkRequest.builder()
+                // 消息列表
+                .messages(messages)
+                // 模型回答的tokens的最大长度,非必传,取值为[1,4096],默认为2048
+                .maxTokens(2048)
+                // 核采样阈值。用于决定结果随机性,取值越高随机性越强即相同的问题得到的不同答案的可能性越高 非必传,取值为[0,1],默认为0.5
+                .temperature(0.2)
+                // 指定请求版本 这个版本根据自己的 API 版本进行修改
+                .apiVersion(SparkApiVersion.V3_5)
+                .build();
+        // 同步调用
+        SparkSyncChatResponse chatResponse = sparkClient.chatSync(sparkRequest);
+        String responseContent = chatResponse.getContent();
+        return responseContent;
+    }
+
 
 }
--- src/test/java/com/yupi/springbootinit/manager/AiManagerTest.java
+++ src/test/java/com/yupi/springbootinit/manager/AiManagerTest.java
@@ -17,4 +17,10 @@ class AiManagerTest {
         String result = aiManager.doChat(1671545080793600002L,"今天有点不开心宝宝");
         System.out.println(result);
     }
+
+    @Test
+    void xinghuo(){
+        String result = aiManager.sendMesToAIUseXingHuo("请问你是什么模型？");
+        System.out.println(result);
+    }
 }
